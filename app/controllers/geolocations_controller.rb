b98e9e538051701109100cc3d785937756303532
MsUzoAgu <uzolearns@gmail.com>
2014-01-16 17:32:36 -0500

    add geolocator gem, bundle install, create view files for geolocator, author actions for geolocations controller

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -1,6 +1,59 @@
 class GeolocationsController < ActionController::Base
   
   def index
-    @geolocations = Geolocation.all 
+    if params[:search].present?
+      @locations = Geolocation.near(params[:search], 50, :order => :distance )
+    else
+      @locations = Geolocation.all
+    end
+
+  def show 
+    @location = Geolocation.find(params[:id])
+  end
+
+  def new
+    @location = Geolocation.new
+  end
+
+  def edit
+    @location = Geolocation.find(params[:id])
+  end
+
+  def create
+    @location = Geolocation.new(params[:location])
+
+    respond_to do |format|
+      if @location.save
+        format.html { redirect_to @location, notice:  'Location was successfully create.'}
+        format.json { render json: @location, status: :created, location: @location }
+      else
+        format.html { render action: "new"}
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @location = Geolocation.find(params[:id])
+
+    respond_to do |format|
+      if @location.update_attribute(params[:location])
+        format.html { redirect_to @location, notice:  'Location was successfully updated.'}
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @location = Geolocation.find(params[:id])
+    @location.destroy
+
+    respond_to do |format|
+      format.html { redirect_to location_url }
+      format.json { head :no_content }
+    end
   end
 end
--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -1,4 +1,8 @@
 class Geolocation < ActiveRecord::Base
   attr_accessible :address, :borough, :latitude, :longitude, :neighborhood
   has_many :scenes
+
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
+
 end
