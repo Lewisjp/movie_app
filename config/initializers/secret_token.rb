a2189f0c4c48b14dabef7dafadd842cbb9a22108
Keturah Walters <your.ketwalters@gmail.com>
2014-01-28 11:49:58 -0500

    add figaro

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MovieApp::Application.config.secret_token = 'fd11c98ee477cad8087df54ddd59f8b8dd55c91d8962a032bf3740e26e479cc43eaa6f4495f71c8ea8d877f431f0d42bea94ae769e69d8df8bd43e79d0cd6a13'
+MovieApp::Application.config.secret_token = ENV['SECRET_TOKEN']
